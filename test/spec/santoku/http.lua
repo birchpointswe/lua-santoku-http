0742b835aefaba5cd6c65e2b151b29a64741dea7
broma0 <121064585+broma0@users.noreply.github.com>
2025-02-26 14:51:18 +0000

    http.lua: update M.fetch, M.get_request, M.post_request, M.request +2
    lib/santoku/http.lua
      + M.get_request, M.post_request, fetch
      - fetch_request
      ~ M.fetch, M.request
    
    also: make.lua, test/spec/santoku/http.lua

--- test/spec/santoku/http.lua
+++ test/spec/santoku/http.lua
@@ -1,6 +1,3 @@
-local test = require("santoku.test")
-local http = require("santoku.http")
-local serialize = require("santoku.serialize")
 
 
 
@@ -30,17 +27,20 @@ local serialize = require("santoku.serialize")
 
 
 
-test("client.get", function ()
-  local client = http.client()
-  client.on("request", function (k, r)
-    return k(r)
-  end, true)
-  print(serialize(client.get("http://localhost:8000/test.json", function (...)
-    local resp = require("santoku.error").checkok(...)
-    return resp
-  end)))
 
 
 
 
-end)
+
+
+
+
+
+
+
+
+
+
+
+
+
